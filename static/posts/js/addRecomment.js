adad31c88274a69b091ef087ed8c7e998824169a
eunchae619 <eunchae619@naver.com>
2020-09-01 21:10:21 +0900

    -

--- static/posts/js/addRecomment.js
+++ static/posts/js/addRecomment.js
@@ -1,3 +1,13 @@
 const addRecoment = (pk) => {
-  console.log(pk);
+  const targetButton = document.querySelector(".jsTargetRecommentButton");
+  const submitBox = document.querySelector("#id_body");
+  if (targetButton.classList.contains("jsAddtargeting")) {
+    targetButton.classList.remove("jsAddtargeting");
+    targetButton.innerText = "답글 쓰기";
+    submitBox.classList.remove("set_border_brown");
+  } else {
+    targetButton.classList.add("jsAddtargeting");
+    targetButton.innerText = "답글중";
+    submitBox.classList.add("set_border_brown");
+  }
 };
